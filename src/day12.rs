3944446009ecec857d93938d593fd0963d67c0bf
Daniel Perez <daniel@perez.sh>
2022-12-27 23:10:15 +0000

    Improve sped for day 12

--- src/day12.rs
+++ src/day12.rs
@@ -17,6 +17,22 @@ impl Day12 {
         let end = *grid.cells.iter().find(|(_, c)| **c == 'E').unwrap().0;
         Box::new(Self { grid, end })
     }
+}
+
+pub struct Env {
+    grid: Grid<char>,
+    cache: HashMap<Point, u64>,
+    end: Point,
+}
+
+impl Env {
+    fn new(grid: Grid<char>, end: Point) -> Self {
+        Self {
+            grid,
+            cache: HashMap::new(),
+            end,
+        }
+    }
 
     fn get_cell(&self, point: &Point) -> Option<char> {
         match self.grid.get(point) {
@@ -39,17 +55,33 @@ impl Day12 {
         });
         neighbors
     }
+    fn update_cache(&mut self, start: &Point, origin: &HashMap<Point, Point>) {
+        let mut current = &self.end;
+        let mut i = 0;
+        while current != start {
+            self.cache.insert(*current, i);
+            current = origin.get(current).unwrap();
+            i += 1;
+        }
+    }
 
-    fn compute_cheapest_path(&self, start: &Point) -> Option<u64> {
+    fn compute_cheapest_path(&mut self, start: &Point) -> Option<u64> {
         let mut open_set = HashSet::from([*start]);
         let mut cheapest_cost = HashMap::from([(*start, 0)]);
         let mut estimated_cost = HashMap::from([(*start, start.manhattan_distance(&self.end))]);
+        let mut origin = HashMap::new();
         while !open_set.is_empty() {
             let current = *open_set
                 .iter()
                 .min_by_key(|p| estimated_cost.get(p).unwrap())
                 .unwrap();
+
+            if let Some(v) = self.cache.get(&current) {
+                return Some(cheapest_cost.get(&current).unwrap() + *v);
+            }
+
             if current == self.end {
+                self.update_cache(start, &origin);
                 return Some(*cheapest_cost.get(&current).unwrap());
             }
 
@@ -63,26 +95,30 @@ impl Day12 {
                         *neighbor,
                         new_score + neighbor.manhattan_distance(&self.end),
                     );
+                    origin.insert(*neighbor, current);
                     open_set.insert(*neighbor);
                 }
             }
         }
         None
     }
+
 }
 
 impl Day for Day12 {
     fn solve_a(&self) -> u64 {
         let start = self.grid.cells.iter().find(|(_, c)| **c == 'S').unwrap().0;
-        self.compute_cheapest_path(start).unwrap()
+        let mut env = Env::new(self.grid.clone(), self.end);
+        env.compute_cheapest_path(start).unwrap()
     }
 
     fn solve_b(&self) -> u64 {
+        let mut env = Env::new(self.grid.clone(), self.end);
         self.grid
             .cells
             .iter()
             .filter(|(_, c)| **c == 'a')
-            .filter_map(|(p, _)| self.compute_cheapest_path(p))
+            .filter_map(|(p, _)| env.compute_cheapest_path(p))
             .min()
             .unwrap()
     }
